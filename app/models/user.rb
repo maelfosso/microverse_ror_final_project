f39b766262567dc214af28a516de3bfb66fa7246
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-06-21 14:19:58 +0100

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  has_many :friend_requests_sent, foreign_key: :from_user, class_name: "FriendRequest"
-  has_many :friend_requests_received, foreign_key: :to_user, class_name: "FriendRequest"
+  has_many :friend_requests_sent, foreign_key: :from_user_id, class_name: "FriendRequest"
+  has_many :users_sent_to, through: :friend_requests_sent, source: :from_user
+
+  has_many :friend_requests_received, foreign_key: :to_user_id, class_name: "FriendRequest"
+  has_many :users_received_from, through: :friend_requests_received, source: :to_user
 end
