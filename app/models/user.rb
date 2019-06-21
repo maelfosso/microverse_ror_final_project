d916de588be9b95e048e5f40e019599522ecf21a
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-06-21 14:58:19 +0100

    Update user_model: Changing some fields names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :friend_requests_sent, foreign_key: :from_user_id, class_name: "FriendRequest"
-  has_many :users_sent_to, through: :friend_requests_sent, source: :from_user
+  has_many :sent_friend_requests, foreign_key: :from_user_id, class_name: "FriendRequest"
+  has_many :sent_friend_requests_users, through: :sent_friend_requests, source: :from_user
 
-  has_many :friend_requests_received, foreign_key: :to_user_id, class_name: "FriendRequest"
-  has_many :users_received_from, through: :friend_requests_received, source: :to_user
+  has_many :received_friend_requests, foreign_key: :to_user_id, class_name: "FriendRequest"
+  has_many :received_friend_requests_users, through: :received_friend_requests, source: :to_user
 end
