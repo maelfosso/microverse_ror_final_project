4ec073c09e5f11d6946b2d95517e1c3ab591a974
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-07-12 10:04:27 +0100

    User model update and Omniauth implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'session/create'
-  get 'session/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'users/sign_out', to: redirect('devise/session#destroy', method: :delete)
 
   get 'auth/facebook/callback', to: 'session#create'
   get 'auth/failure', to: 'session#failure'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_070140) do
+ActiveRecord::Schema.define(version: 2019_07_12_021946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,11 @@ ActiveRecord::Schema.define(version: 2019_07_08_070140) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider"
+    t.string "uid"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.string "photo_path"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
