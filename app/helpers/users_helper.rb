6294618facd934cffa5f67a6e4f9258ca86ddcbc
FabienNeibaf <fabienlintone@gmail.com>
2019-08-08 22:23:09 +0300

    Fix photo_path helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
   def photo_path(user)
-    user.photo_path || user.photo_path = "avatars/#{rand 1..4}.png"
+    user.photo_path ||
+    (user.update(photo_path: "avatars/#{rand 1..4}.png") && user.photo_path)
   end
 
   def countries
