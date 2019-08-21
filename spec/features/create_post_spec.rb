6973eaac2babac5f48f16158c7e0d5182855c741
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-08-21 13:29:52 +0100

    Wait a bit

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Post creation', type: :feature do
+RSpec.feature 'Post creation', type: :feature, js: true do
 	before do
 		@user = create(:user)
 		login_as @user 
@@ -11,7 +11,7 @@ RSpec.feature 'Post creation', type: :feature do
 		
 		fill_in "post_title", with: "Some more text"
 		fill_in "post_content", with: "Some more content"
-		click_on "Share"
+		click_on "Share", xhr: true
 		
 		visit users_path(@user)
 
