f895042359d0d2982e50f9ffd9622071a5277255
FabienNeibaf <fabienlintone@gmail.com>
2019-08-21 15:22:04 +0300

    Fix missing template error

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.feature 'Post creation', type: :feature do
+RSpec.feature 'Post creation' do
 	before do
 		@user = create(:user)
-		login_as @user 
-	end 
+		login_as @user
+	end
 
-	scenario 'Successful creation' do
+	scenario 'Successful creation', :js do
 		visit root_path
-		
+
 		fill_in "post_title", with: "Some more text"
 		fill_in "post_content", with: "Some more content"
 		click_on "Share"
-		
+
 		visit users_path(@user)
 
 		expect(page).to have_content "Some more content"
