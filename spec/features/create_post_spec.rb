4bb822191b25272479883c122ecda148b77da751
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-08-26 12:31:06 +0100

    Add 2 or 3 integration test

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -1,28 +1,3 @@
-# require 'rails_helper'
-
-
-# RSpec.feature 'Post creation' do
-# 	before do
-# 		@user = create(:user)
-# 		login_as @user
-# 	end
-
-# 	scenario 'Successful creation', :js do
-# 		visit root_path
-# 		# save_and_open_page
-
-# 		fill_in "post[title]", with: "Some more text"
-# 		fill_in "post[content]", with: "Some more content"
-# 		# wait_until { !page.find("Share").visible? }
-# 		click_button "Share"
-
-# 		# visit root_path
-# 		# save_and_open_page
-# 		find('.post')
-# 		visit user_path(@user)
-# 		expect(page).to have_content "Some more text"
-# 	end
-# end
 require 'rails_helper'
 
 RSpec.feature 'Post creation' do
